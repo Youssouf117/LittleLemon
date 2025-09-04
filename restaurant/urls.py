c0ce65316da0520eb8de88f49e3a1aeb0ee86ce8
Youssouf Siraba <ykon2030@gmail.com>
2025-09-04 10:40:42 +0200

    creer des API

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,8 +1,14 @@
 #define URL route for index() view
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+
+router=DefaultRouter()
+router.register(r'Booking', views.BookingViewSet)
 
 urlpatterns = [
-    
-    path('', views.index, name='index')
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
+    path('', include(router.urls)),
 ]
