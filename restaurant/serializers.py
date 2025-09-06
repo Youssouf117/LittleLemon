6b908025f24ff1ebbfcfe63fb6ddda421ff459a8
Youssouf Siraba <ykon2030@gmail.com>
2025-09-06 09:54:19 +0200

    authentication of my API by Insomnia

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,4 +1,13 @@
+from rest_framework import serializers
+from .models import MenuItem
 
+
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=MenuItem
+        fields='__all__'
 
 
         
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
 
-# Create your views here.
+@api_view()
+@permission_classes([IsAuthenticated])
+def msg(request):
+    return Response({"message":"This view is protected"})
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+  permission_classes = [IsAuthenticated]
+  queryset = MenuItem.objects.all()
+  serializer_class = MenuItemSerializer
 
     
 
